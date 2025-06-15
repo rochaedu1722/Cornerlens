2e4fa673f676636c8512d804559e932adab01fb6
rochaedu1722 <rochaedu@icloud.com>
2025-06-15 15:56:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ if __name__ == "__main__":
             print("🟢 [LOG] Varredura concluída com sucesso.", flush=True)
         except Exception as e:
             print(f"🔴 [ERRO] Falha durante a varredura: {e}", flush=True)
-        print("⏳ Aguardando 15 minutos para nova varredura...\n", flush=True)
-        time.sleep(900)
+
+        print("⏳ Aguardando 15 minutos para nova varredura...", flush=True)
+        for i in range(15, 0, -1):
+            print(f"⏳ ...{i} minuto(s) restante(s)", flush=True)
+            time.sleep(60)
